bbf60c4da2bf7049de0bd3d22ec9ba486392c08a
vikas prajapat <vikasprajapat34118@gmail.com>
2025-11-26 13:48:33 +0530

    updates a project section

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)  # Fixed: __name__ instead of name
 app.config['SECRET_KEY'] = os.urandom(32)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
+
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USER')      # e.g., your_email@gmail.com
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASS')      # App Password (NOT regular password)
